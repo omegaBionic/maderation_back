2d8b725ed2c1cf835415534c0942565bad0a5032
omegaBionic <44264108+omegaBionic@users.noreply.github.com>
2019-10-16 16:10:53 +0200

    [Main] Added comments into index for a better readability.

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
+/* REQUIREMENTS */
 var users = require('./get/users');
 
+/* MODULS AND SETUP */
 var express = require('express');
 var http = require('http');
 var url = require('url');
 var querystring = require('querystring');
 var AWS = require('aws-sdk');
 
+/* start the http listening server */
 var app = express();
 
+/* aws connection */
 var db = new AWS.DynamoDB({'region': 'eu-west-3'});
 
+/* getUser() */
 app.get('/API/getUser', function(req, res) {
   res = users.getUser(db, url, req, res)
 })
+
+/* if bad answer by getUser() */
 .use(function(req, res, next){
     res.setHeader('Content-Type', 'text/plain');
     res.status(404).send('Page introuvable !');
