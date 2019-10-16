fa8ae0fe3a66863a19f9749b8142010c2f82d08a
omegaBionic <44264108+omegaBionic@users.noreply.github.com>
2019-10-16 16:33:56 +0200

    [Get] Added comments for a better readability.

--- get/users.js
+++ get/users.js
@@ -1,14 +1,18 @@
 var querystring = require('querystring');
 var url = require('url');
 
-/* this function is used for get user */
 module.exports = {
+  /* this function is used for get user */
   getUser : function (db, url, req, res){
     console.log("request received into getUser function.");
+
+    /* parse datas */
+    console.log("parse datas.");
     var params = querystring.parse(url.parse(req.url).query);
     console.log("username: " + params['username']);
     console.log("password: " + params['password']);
 
+    /* check username and password */
     if ('username' in params && 'password' in params) {
       var paramsdb = {
         Key: {
@@ -18,15 +22,16 @@ module.exports = {
         },
         TableName: "login_melo"
       };
+      /* launch request for username and password */
       db.getItem(paramsdb, function(err, data) {
         if (err){
           console.log(err, err.stack);
-        } else {// an error occurred
+        } else {
           console.log("datas received from database: ");
           console.log(data);
           res.setHeader('Content-Type', 'application/json');
           res.json(data);
-        }// successful response
+        }
       });
     }
     return res;
