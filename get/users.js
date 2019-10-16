66acc0383e43065c2d9226704bb0f327ce323c3a
omegaBionic <44264108+omegaBionic@users.noreply.github.com>
2019-10-16 16:27:58 +0200

    [Get] Changed default indentation to 2 spaces for unify the code.

--- get/users.js
+++ get/users.js
@@ -3,31 +3,30 @@ var url = require('url');
 
 /* this function is used for get user */
 module.exports = {
-    getUser : function (db, url, req, res){
+  getUser : function (db, url, req, res){
     console.log("request received into getUser function.");
     var params = querystring.parse(url.parse(req.url).query);
     console.log("username: " + params['username']);
     console.log("password: " + params['password']);
-  
+
     if ('username' in params && 'password' in params) {
       var paramsdb = {
         Key: {
         "username": {
-          S: params['username']
-          }
+        S: params['username']
+        }
         },
         TableName: "login_melo"
       };
       db.getItem(paramsdb, function(err, data) {
         if (err){
           console.log(err, err.stack);
-        }// an error occurred
-        else {
+        } else {// an error occurred
           console.log("datas received from database: ");
           console.log(data);
           res.setHeader('Content-Type', 'application/json');
           res.json(data);
-        }        // successful response
+        }// successful response
       });
     }
     return res;
