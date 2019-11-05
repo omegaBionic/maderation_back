37ce9c1299dcd0771075b6602cfc964314f703f1
omegaBionic <44264108+omegaBionic@users.noreply.github.com>
2019-11-05 10:47:53 +0100

    MDDEV-19 #[Get] Changed users return code and description.

--- get/users.js
+++ get/users.js
@@ -2,27 +2,24 @@ let querystring = require('querystring');
 let url = require('url');
 let Logger = require('../utils/logger')
 let logger = new Logger("users", "debug")
-let Token = require('../utils/token')
 
 module.exports = {
-  /* this function is used for get user */
+  /* this function is used for get users */
   getUser : function (db, url, req, res){
-    let token = new Token("omegaToken");
-
-    logger.debug("request received into getUser function.");
+    logger.debug("request received into getusers function.");
 
     /* parse datas */
     logger.debug("parse datas.");
     let params = querystring.parse(url.parse(req.url).query);
     logger.debug("key: " + params['key']);
 
-    /* check username and password */
+    /* check users and password */
     if ('key' in params) {
       if (params['key'] == "83c2c07ea1251a1a39ec46d52cbba19c"){
         let paramsdb = {
           TableName: "madera_user"
         };
-        /* launch request for username and password */
+        /* launch request for users and password */
         db.scan(paramsdb, function(err, data) {
           if (err){
             console.log(err, err.stack);
@@ -30,26 +27,28 @@ module.exports = {
           } else {
             logger.info("datas received from database");
             res.setHeader('Content-Type', 'application/json');
-            res.send({
-              status: true,
+            res.status(500).send({
+              status: 500,
               datas: data
-              });
+            });
             logger.debug("datas return ok for true status");
           }
         });
       } else { // if bad key
         logger.info("bad bad_key.");
-        res.send({
-          status: false,
-          datas: 'Error: bad_key'
-          });
+        res.setHeader('Content-Type', 'application/json');
+        res.status(500).send({
+          status: 500,
+          datas: 'Error: bad key'
+        });
       }
     } else { // if bad parameter
       logger.info("bad bad_parameter.");
-      res.send({
-        status: false,
-        datas: 'Error: bad_parameter'
-        });
+      res.setHeader('Content-Type', 'application/json');
+      res.status(500).send({
+        status: 500,
+        datas: 'Error: bad parameter'
+      });
     }
     return res;
   }
