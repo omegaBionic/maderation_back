d3cdef0decdd48c67cb308a1622d640ab6b222ce
omegaBionic <44264108+omegaBionic@users.noreply.github.com>
2019-11-06 14:09:51 +0100

    MDDEV-19 #[Get] Added test_promotion_comp.

---
+++ tests/get/test_promotion_comp.js
@@ -0,0 +1,27 @@
+var request = require('supertest')('http://localhost:8080');
+
+
+describe('/api/get/promotion_comp', function() {
+        it('returns 200 status - ok', function(done) {
+            this.timeout(10000);
+            request
+                .get('/api/get/promotion_comp?key=2b11565d85da178b3a1942a22d20c624')
+                .expect(200, done);
+        });
+        it('returns 200 status - is json', function(done) {
+            this.timeout(10000);
+            request
+                .get('/api/get/promotion_comp?key=2b11565d85da178b3a1942a22d20c624')
+                .expect('Content-Type', /json/, done);
+        });
+        it('returns 500 status - bad key', function(done) {
+            request
+                .get('/api/get/promotion_comp?key=bad-key_52cbba19c')
+                .expect(500, done);
+        });
+        it('returns 500 status - bad parameter', function(done) {
+            request
+                .get('/api/get/promotion_comp?bad_parameter=83c2c07ea1251a1a39ec46d52cbba19c')
+                .expect(500, done);
+        });
+});
