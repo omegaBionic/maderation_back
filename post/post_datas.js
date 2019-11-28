3a4c6d3ba97e6a1f8f22664303ace9657803f26b
omegaBionic <44264108+omegaBionic@users.noreply.github.com>
2019-11-28 16:45:00 +0100

    MDDEV-37 #[Post] Changed refactor with 2 spaces.

--- post/post_datas.js
+++ post/post_datas.js
@@ -5,93 +5,96 @@ let Logger = require('../utils/logger')
 let logger = new Logger("postDatas", "debug")
 
 module.exports = {
-    /* this function is used for get users */
-    postDatas: function (db, url, req, res, id) {
-        logger.debug("request received into getusers function.");
+  /* this function is used for get users */
+  postDatas: function (db, url, req, res, id) {
+    logger.debug("request received into getusers function.");
 
-        /* parse datas */
-        logger.debug("parse datas.");
-        let params = querystring.parse(url.parse(req.url).query);
-        logger.debug("key: " + params['key']);
+    /* parse datas */
+    logger.debug("parse datas.");
+    let params = querystring.parse(url.parse(req.url).query);
+    logger.debug("key: " + params['key']);
 
-        /* check key and check datas */
-        if ('key' in params) {
-            if (params['key'] == "993b06009dce6a9962esecf49801d32e") {
-                /* get and parse body to jsonBody */
-                let body = '';
-                req.on('data', chunk => {
-                    body += chunk.toString(); // convert Buffer to string
-                });
+    /* check key and check datas */
+    if ('key' in params) {
+      if (params['key'] == "993b06009dce6a9962esecf49801d32e") {
+        /* get and parse body to jsonBody */
+        let body = '';
+        req.on('data', chunk => {
+          body += chunk.toString(); // convert Buffer to string
+        });
 
-                req.on('end', () => {
-                    if (json.isJson(body)) { // check data integrity
-                        logger.debug("inputJson is in json format");
-                        let jsonBody = JSON.parse(body)
-                        for (item in jsonBody) {
-                            console.info("parse bodyJson");
-                            console.debug("item: '" + item + "'");
-                            console.debug("jsonBody[item].status: '" + jsonBody[item].status + "'");
-                            console.debug("jsonBody[item].table: '" + jsonBody[item].table + "'");
-                            console.debug("jsonBody[item].values: '" + JSON.stringify(jsonBody[item].values) + "'");
-                            switch (jsonBody[item].status) {
-                                case 'add':
-                                    logger.debug("into add case");
-                                    /* insert/push datas into dynamodb */
-                                    let paramsdb = {
-                                        TableName: jsonBody[item].table,
-                                        Item: jsonBody[item].values
-                                    };
+        req.on('end', () => {
+          if (json.isJson(body)) { // check data integrity
+            logger.debug("inputJson is in json format");
+            let jsonBody = JSON.parse(body)
+            for (item in jsonBody) {
+              console.info("parse bodyJson");
+              console.debug("item: '" + item + "'");
+              console.debug("jsonBody[item].status: '" + jsonBody[item].status + "'");
+              console.debug("jsonBody[item].table: '" + jsonBody[item].table + "'");
+              console.debug("jsonBody[item].values: '" + JSON.stringify(jsonBody[item].values) + "'");
+              switch (jsonBody[item].status) {
+                case 'add':
+                  logger.debug("into add case");
+                  /* insert/push datas into dynamodb */
+                  let paramsdb = {
+                    TableName: jsonBody[item].table,
+                    Item: jsonBody[item].values
+                  };
 
-                                    db.putItem(paramsdb, function (err, data) {
-                                        if (err) {
-                                            console.log(err, err.stack);
-                                            //res.json(data);
-                                        } else {
-                                            logger.info("datas pushed into database");
-                                        }
-                                    });
-                                    break;
-                                case 'modify':
-                                    logger.debug("into modify case");
-                                    break;
-                                case 'delete':
-                                    logger.debug("into delete case");
-                                    break;
-                                default:
-                                    logger.info("bad status request into json");
-                                    break;
-                            }
-                        }
-                        res.setHeader('Content-Type', 'application/json');
-                        res.status(200).send({
-                            status: 200,
-                            datas: 'datas pushed into database'
-                        });
+                  db.putItem(paramsdb, function (err, data) {
+                    if (err) {
+                      console.log(err, err.stack);
+                      //res.json(data);
                     } else {
-                        logger.info("inputJson is not in json format");
-                        res.setHeader('Content-Type', 'application/json');
-                        res.status(500).send({
-                            status: 500,
-                            datas: 'inputJson is not in json format'
-                        });
+                      logger.info("datas pushed into database");
                     }
-                });
-            } else { // if bad key
-                logger.info("bad bad_key.");
-                res.setHeader('Content-Type', 'application/json');
-                res.status(500).send({
-                    status: 500,
-                    datas: 'Error: bad key'
-                });
+                  });
+                  break;
+                case 'modify':
+                  logger.debug("into modify case");
+                  break;
+                case 'delete':
+                  logger.debug("into delete case");
+                  break;
+                default:
+                  logger.info("bad status request into json");
+                  break;
+              }
             }
-        } else { // if bad parameter
-            logger.info("bad bad_parameter.");
+            console.log("OMEGA");
+            console.log(id);
+
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).send({
+              status: 200,
+              datas: 'datas pushed into database'
+            });
+          } else {
+            logger.info("inputJson is not in json format");
             res.setHeader('Content-Type', 'application/json');
             res.status(500).send({
-                status: 500,
-                datas: 'Error: bad parameter'
+              status: 500,
+              datas: 'inputJson is not in json format'
             });
-        }
-        return res;
+          }
+        });
+      } else { // if bad key
+        logger.info("bad bad_key.");
+        res.setHeader('Content-Type', 'application/json');
+        res.status(500).send({
+          status: 500,
+          datas: 'Error: bad key'
+        });
+      }
+    } else { // if bad parameter
+      logger.info("bad bad_parameter.");
+      res.setHeader('Content-Type', 'application/json');
+      res.status(500).send({
+        status: 500,
+        datas: 'Error: bad parameter'
+      });
     }
+    return res;
+  }
 }
