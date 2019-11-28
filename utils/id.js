24a3ddf1ab7140e20b090fce52e62fe4c6303a6b
omegaBionic <44264108+omegaBionic@users.noreply.github.com>
2019-11-28 16:34:39 +0100

    MDDEV-37 #[Utils] Added id param check.

--- utils/id.js
+++ utils/id.js
@@ -34,22 +34,26 @@ class Id{
     /* get client id */
     logger.debug("parse datas for get id");
     let params = querystring.parse(url.parse(req.url).query);
-    this.idNumber = params["id"];
-    logger.info("input data id: " + this.idNumber);
+    if ("id" in params){
+      logger.debug("id found into param");
+      this.idNumber = params["id"];
+      logger.info("input data id: " + this.idNumber);
     
-    /* check if id exist */
-    if (this.idNumber in this.idJsonObject){
-      logger.debug("idNumber is in json file: OK");
+      /* check if id exist */
+      if (this.idNumber in this.idJsonObject){
+        logger.debug("idNumber is in json file: OK");
+      } else {
+        logger.debug("idNumber is in json file: KO");
+
+        let addToJson = '{"' + this.idNumber + '":{"address_client": true, "address_supplier": true, "all": true, "category": true, "chat": true, "client": true, "component": true, "gamme": true, "invoice_quotation": true, "message": true, "product": true, "project": true, "promotion_cat": true, "promotion_comp": true, "quotation": true, "role": true, "shop": true, "status": true, "stock": true, "supplier": true, "users": true}}';
+        logger.info("addToJson: '" + addToJson + "'");
+
+        Object.assign(this.idJsonObject, JSON.parse(addToJson))
+
+        fs.writeFileSync(this.idFullName, JSON.stringify(this.idJsonObject));
+      }
     } else {
-      logger.debug("idNumber is in json file: KO");
-      //this.idJsonObject.push('"this.idNumber":{"madera_chat":true}');
-
-      let addToJson = '{"' + this.idNumber + '":{"address_client": true, "address_supplier": true, "all": true, "category": true, "chat": true, "client": true, "component": true, "gamme": true, "invoice_quotation": true, "message": true, "product": true, "project": true, "promotion_cat": true, "promotion_comp": true, "quotation": true, "role": true, "shop": true, "status": true, "stock": true, "supplier": true, "users": true}}';
-      logger.info("addToJson: '" + addToJson + "'");
-
-      Object.assign(this.idJsonObject, JSON.parse(addToJson))
-
-      fs.writeFileSync(this.idFullName, JSON.stringify(this.idJsonObject));
+      logger.debug("id not found into param");
     }
   }
 
